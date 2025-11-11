a2ddccaad44a7c7c94ca37d8faa4ca178f655a5f
AsmamawDinku <adinku@byupathway.edu>
2025-11-11 18:16:00 -0500

    issue debugued

--- week01/code/obj/Debug/net8.0/code.AssemblyInfo.cs
+++ week01/code/obj/Debug/net8.0/code.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("code")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+aae4917fedc364390f2b6e6951e3211c972e6acc")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f229b3bce2fb141e567d333d93be59f2ad3c47ce")]
 [assembly: System.Reflection.AssemblyProductAttribute("code")]
 [assembly: System.Reflection.AssemblyTitleAttribute("code")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
